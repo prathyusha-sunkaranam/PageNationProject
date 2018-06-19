78334edb6775cef8643a426c0e5f5cd846d87c0c
prathyusha-sunkaranam <prathyu565@gmail.com>
2018-06-19 11:43:15 +0530

    Initial commit

---
+++ app/src/main/java/com/mansopresk/mansopresk01/pagenationproject/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mansopresk.mansopresk01.pagenationproject;
+
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
+import com.commit451.endlessrecyclerviewonscrolllistener.EndlessRecyclerViewOnScrollListener;
+import com.mansopresk.mansopresk01.pagenationproject.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> mStringList;
+    private int mLoadedItems = 0;
+    private SampleAdapter mSampleAdapter;
+    private ActivityMainBinding mActivityMainBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mActivityMainBinding.recyclerView.setLayoutManager(linearLayoutManager);
+        mActivityMainBinding.recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
+        mStringList = new ArrayList<>();
+        mSampleAdapter = new SampleAdapter(mStringList);
+        mActivityMainBinding.recyclerView.setAdapter(mSampleAdapter);
+        addDataToList();
+
+        mActivityMainBinding.recyclerView.addOnScrollListener(new EndlessRecyclerViewOnScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore() {
+                addDataToList();
+
+            }
+        });
+    }
+
+    private void addDataToList() {
+        mActivityMainBinding.itemProgressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 30; i++) {
+                    mStringList.add("Prathyusha : " + mLoadedItems);
+                    mLoadedItems++;
+                }
+                mSampleAdapter.notifyDataSetChanged();
+                mActivityMainBinding.itemProgressBar.setVisibility(View.GONE);
+            }
+        }, 1500);
+
+    }
+    }
+
